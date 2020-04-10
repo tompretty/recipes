cc45b6e7bdc555a4f89478eaddc65f532e82dcc0
TomPretty <tompretty2@gmail.com>
2020-04-10 21:33:25 +0100

    Add tags to recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -18,6 +18,6 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name, :url, :image_url)
+    params.require(:recipe).permit(:name, :url, :image_url, :tag_list)
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,6 @@ class Recipe < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :name, :url, :image_url
+
+  acts_as_taggable
 end
