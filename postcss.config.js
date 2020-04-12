474f15e681d1f008c42778a7f6db289a14b553ca
TomPretty <tompretty2@gmail.com>
2020-04-12 16:15:21 +0100

    Add purgecss for production

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,16 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./app/**/*.html.erb", "./app/helpers/**/*.rb"],
+
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
   plugins: [
     require("postcss-import"),
     require("postcss-flexbugs-fixes"),
     require("tailwindcss"),
     require("autoprefixer"),
+    ...(process.env.RAILS_ENV == "production" ? [purgecss] : []),
     require("postcss-preset-env")({
       autoprefixer: {
         flexbox: "no-2009"
