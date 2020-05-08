0966b2aac501ee420f32d08fcecedffd20ec67df
TomPretty <tompretty2@gmail.com>
2020-05-08 14:05:53 +0100

    Style tags filter and page header

---
+++ app/helpers/recipes_helper.rb
@@ -0,0 +1,35 @@
+module RecipesHelper
+  def all_filter_tag
+    render partial: "tag", locals: locals_for_all_tag
+  end
+
+  def filter_tag(tag)
+    render partial: "tag", locals: locals_for_tag(tag)
+  end
+
+  private
+
+  def locals_for_all_tag
+    { name: "all", count: current_user.recipes.count, link: link_for_all_tag, is_active: all_tag_is_active? }
+  end
+
+  def link_for_all_tag
+    request.params.except(:tag)
+  end
+
+  def all_tag_is_active?
+    request.params[:tag].nil? 
+  end
+
+  def locals_for_tag(tag)
+    { name: tag.name, count: tag.count, link: link_for_tag(tag), is_active: tag_is_active?(tag) }
+  end
+
+  def link_for_tag(tag)
+    request.params.merge(tag: tag.name)
+  end
+
+  def tag_is_active?(tag)
+    request.params[:tag] == tag.name
+  end
+end
